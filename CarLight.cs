53bff9a9d373e90c006d5314d299dc4eb41633b4
Arun <arunknow1992@gmail.com>
2016-12-09 20:08:22 +0530

    turns light on

---
+++ CarLight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarLight : MonoBehaviour {
+
+	public Renderer brakelights;
+	public Material brakelightON;
+	public Material brakelightsOFF;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.DownArrow))
+			brakelights.material = brakelightON;
+		else
+			brakelights.material = brakelightsOFF;
+	}
+}
